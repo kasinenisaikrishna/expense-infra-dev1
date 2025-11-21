fc620fa82651d73d9db44d35e6760834b0d41d8c
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-21 17:54:51 +0530

    terraform

--- 20-sg/parameters.tf
+++ 20-sg/parameters.tf
@@ -35,3 +35,8 @@ resource "aws_ssm_parameter" "app_alb_sg_id" {
   value = module.app_alb_sg.id
 }
 
+resource "aws_ssm_parameter" "vpn_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
+  type  = "String"
+  value = module.vpn_sg.id
+}
