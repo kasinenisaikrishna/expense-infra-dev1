30b4a07b25f8584bf72c00535e0d5527b9ad918f
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-21 19:31:56 +0530

    terraform

--- 60-vpn/data.tf
+++ 60-vpn/data.tf
@@ -10,7 +10,7 @@ data aws_ssm_parameter "public_subnet_ids" {
 data "aws_ami" "joindevops" {
     
     most_recent = true
-    owners = ["973714476881"]
+    owners = ["679593333241"]
     
     filter {
         name   = "name"
